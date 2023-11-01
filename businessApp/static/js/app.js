93741e65883999b6ce6931a55db2948c58126d91
Christian Cleofas <c.cleofas@psp.team>
2023-11-01 18:30:23 +0800

    NO_TICKET - Create function

--- businessApp/static/js/app.js
+++ businessApp/static/js/app.js
@@ -1,11 +1,11 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
+// const sign_in_btn = document.querySelector("#sign-in-btn");
+// const sign_up_btn = document.querySelector("#sign-up-btn");
 
-const container = document.querySelector(".container");
+// const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener('click', () => {
-    container.classList.add("sign-up-mode");
-});
-sign_in_btn.addEventListener('click', () => {
-    container.classList.remove("sign-up-mode");
-});
+// sign_up_btn.addEventListener('click', () => {
+//     container.classList.add("sign-up-mode");
+// });
+// sign_in_btn.addEventListener('click', () => {
+//     container.classList.remove("sign-up-mode");
+// });
