ee2fe55e33d4db238c6db6cfff63222435bbb6c2
Christian Cleofas <c.cleofas@psp.team>
2023-11-01 18:30:23 +0800

    NO_TICKET - Create function

--- businessApp/urls.py
+++ businessApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from businessApp import views
 
 urlpatterns = [
-    path('', views.mainpage, name='mainpage'),
+    path('', views.home, name='home'),
+    path('login_page', views.login_page, name='login_page'),
+    path('register', views.register, name='register'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- businessApp/views.py
+++ businessApp/views.py
@@ -1,6 +1,49 @@
-from django.shortcuts import render
+# views.py
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
-
-def mainpage(request):
+def home(request):
     return render(request, 'businessApp/index.html')
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST['login-username']
+        password = request.POST['login-password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # fname = user.first_name
+            messages.success(request, "Logged In Sucessfully!!")
+            # return render(request, 'authentication/dashboard.html')
+            # return render(request, "authentication/dashboard.html",{"fname":fname})
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return redirect('dashboard')
+
+    return render(request, 'businessApp/login.html')
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('signup-username')
+        email = request.POST.get('signup-email')
+        password = request.POST.get('signup-password')
+
+        myuser = User.objects.create_user(username, email, password)
+        myuser.save()
+
+        # Add a context variable to indicate successful registration
+        context = {
+            'registration_successful': True,
+        }
+
+        return render(request, 'businessApp/register.html', context)
+
+    return render(request, 'businessApp/register.html')
+
+def dashboard(request):
+    return render(request, 'businessApp/dashboard.html')
